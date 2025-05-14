51b05d61ba62d514dca3b7578d329c6bf61cae64
tata-vladi <tatiana20121990v@gmail.com>
2025-05-14 17:26:49 +0300

    8lesson новый

--- 08_lesson/test_yogile_api.py
+++ 08_lesson/test_yogile_api.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 Base_URL = 'https://ru.yougile.com/api-v2/'
-api_key = "My_api"  # заменить на мой API ключ
+api_key = "N245iqt+tGKSmGMVq6dlY9aKS-ZfYlpB-bMYIVoeCX1CDDi+5bu8KYAykbdjMNht"  # заменить на мой API ключ
 
 
 @pytest.fixture
@@ -42,33 +42,41 @@ def test_create_project_negative():
         "users": {}
     }
     resp = requests.post(Base_URL + 'projects', headers=headers, json=data)
-    assert resp.status_code == 201
+    assert resp.status_code == 400
 
 
-def test_update_project_positive():
-    created_project = api.create_project("Тестовый Проект", users_info)
-    project_id = created_project.json()["id"]
-    new_title = "Новое название проекта"
-    response = api.update_project(project_id, new_title)
-    assert response.status_code == 200
-    updated_project = api.get_project(project_id)
-    assert updated_project.json()["title"] == new_title
-
-def test_update_project_invalid_data():
-    created_project = api.create_project("Тестовый Проект", users_info)
-    project_id = created_project.json()["id"]
-    response = api.update_project(project_id, "")
-    assert response.status_code == 400
-
-
-
-def test_get_project_positive(create_project):
+def test_update_project_positive(create_project):   #провал
     project_id = create_project
     headers = {
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json",
     }
-    resp = requests.get(f'{Base_URL} + projects/{project_id}', headers=headers)
+    data = {"title": "Update test title"}
+    resp = requests.put(f'{Base_URL}projects/{project_id}',
+                        headers=headers, json=data)
+    assert resp.status_code == 200
+
+
+
+def test_update_project_negative(create_project):
+    project_id = "non_existent_project_id"
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+    data = {"title": "Update test title"}
+    resp = requests.put(f'{Base_URL}  projects/{project_id}',
+                        headers=headers, json=data)
+    assert resp.status_code == 404
+
+
+def test_get_project_positive(create_project):  # провал
+    project_id = create_project
+    headers = {
+        "Authorization": f"Bearer {api_key}",
+        "Content-Type": "application/json",
+    }
+    resp = requests.get(f'{Base_URL} projects/{project_id}', headers=headers)
     assert resp.status_code == 200
 
 
@@ -78,5 +86,5 @@ def test_get_project_negative():
         "Authorization": f"Bearer {api_key}",
         "Content-Type": "application/json",
     }
-    resp = requests.get(f'{Base_URL} + projects/{project_id}', headers=headers)
+    resp = requests.put(f'{Base_URL} + projects/{project_id}', headers=headers)
     assert resp.status_code == 404
