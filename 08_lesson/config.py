208415b59353af1c7b58da8f4ee2e4eea85ecb41
tata-vladi <tatiana20121990v@gmail.com>
2025-05-19 15:25:16 +0300

    8lesson новый

--- 08_lesson/config.py
+++
@@ -1,8 +0,0 @@
-api_url = "https://yougile.com"
-my_headers = {'Authorization': 'Bearer Zbq0TZZNnlG062A4ci+d4mUI1LR2zQFXd5tOZBbw9kyTBNbNofT7qwZGA1tKa+i5',
-           "Content-Type": "application/json"}
-not_valid_headers = {'Authorization': 'Bearer -',
-           "Content-Type": "application/json"}
-users_info = {
-    "a3dc935a-47b4-4fa6-8aa3-3428dd2e7608": "admin"
-}
